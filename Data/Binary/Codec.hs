3b29ab134dc52d98c4eff3dc0a8cc31c43cb8df4
Patrick Chilton <chpatrick@gmail.com>
2015-05-02 20:20:18 +0100

    Complete Data.Binary.Codec.

--- Data/Binary/Codec.hs
+++ Data/Binary/Codec.hs
@@ -4,6 +4,10 @@ module Data.Binary.Codec
     BinaryCodec
   , byteString
   , word8
+  , word16be, word16le, word16host
+  , word32be, word32le, word32host
+  , word64be, word64le, word64host
+  , wordhost
   -- * Testing
   , reversible
   )
@@ -27,9 +31,38 @@ byteString n = Codec
       else fail "ByteString wrong size for field."
   }
 
--- | Get/put a byte.
 word8 :: BinaryCodec Word8
 word8 = Codec getWord8 putWord8
+
+word16be :: BinaryCodec Word16
+word16be = Codec getWord16be putWord16be
+
+word16le :: BinaryCodec Word16
+word16le = Codec getWord16le putWord16le
+
+word16host :: BinaryCodec Word16
+word16host = Codec getWord16host putWord16host
+
+word32be :: BinaryCodec Word32
+word32be = Codec getWord32be putWord32be
+
+word32le :: BinaryCodec Word32
+word32le = Codec getWord32le putWord32le
+
+word32host :: BinaryCodec Word32
+word32host = Codec getWord32host putWord32host
+
+word64be :: BinaryCodec Word64
+word64be = Codec getWord64be putWord64be
+
+word64le :: BinaryCodec Word64
+word64le = Codec getWord64le putWord64le
+
+word64host :: BinaryCodec Word64
+word64host = Codec getWord64host putWord64host
+
+wordhost :: BinaryCodec Word
+wordhost = Codec getWordhost putWordhost
 
 -- | Verify that a `BinaryCodec` is reversible for a given input.
 reversible :: Eq a => BinaryCodec a -> a -> Bool
