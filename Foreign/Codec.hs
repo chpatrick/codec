6e399719366936ab4fd790aa749a3aed7e6afde0
Patrick Chilton <chpatrick@gmail.com>
2015-05-03 03:47:33 +0100

    Add Foreign codec.

---
+++ Foreign/Codec.hs
@@ -0,0 +1,33 @@
+module Foreign.Codec
+  ( ForeignContext, ForeignCodec, ForeignCodec'
+    , peekCodec, pokeCodec, field
+    ) where
+
+import Control.Monad.Reader
+import Foreign
+
+import Data.Codec.Codec
+
+type ForeignContext a = ReaderT (Ptr a) IO
+-- | A foreign codec for @a@ given a pointer to @p@.
+type ForeignCodec' p a = Codec (ForeignContext p) (ForeignContext p) a
+-- | A foreign pointer for @a@ given a pointer to itself.
+type ForeignCodec a = ForeignCodec' a a
+
+-- | Peek a value using a `ForeignCodec'`.
+peekCodec :: ForeignCodec' p a -> Ptr p -> IO a
+peekCodec (Codec r _)
+  = runReaderT r
+
+-- | Poke a value using a `ForeignCodec`.
+pokeCodec :: ForeignCodec' p a -> Ptr p -> a -> IO ()
+pokeCodec (Codec _ w) ptr x
+  = runReaderT (w x) ptr
+
+-- | A codec for a field of a foreign structure, given its byte offset.
+-- You can get an offset easily using @{#offset struct_type, field}@ with @hsc2hs@.
+field :: Storable a => Int -> ForeignCodec' p a
+field off = Codec
+  { parse = ReaderT (`peekByteOff` off)
+  , produce = \x -> ReaderT $ \ptr -> pokeByteOff ptr off x
+}
---
+++ examples/Foreign.hsc
@@ -0,0 +1,27 @@
+module Data.Codec.Examples.Foreign where
+
+#include "time.h"
+
+#let alignment t = "%lu", (unsigned long)offsetof(struct {char x__; t (y__); }, y__)
+
+import Foreign
+import Data.Codec
+import Foreign.Codec
+
+data TimeSpec = TimeSpec
+  { seconds :: Int
+  , nanoseconds :: Int
+  }
+
+genFields ''TimeSpec
+
+timeSpecCodec :: ForeignCodec TimeSpec
+timeSpecCodec = finish TimeSpec
+  $   f_seconds     >-< field (#offset struct timespec, tv_sec)
+  >>> f_nanoseconds >-< field (#offset struct timespec, tv_nsec)
+
+instance Storable TimeSpec where
+  sizeOf _ = #{size struct timespec}
+  alignment _ = #{alignment struct timespec}
+  peek = peekCodec timeSpecCodec
+  poke = pokeCodec timeSpecCodec
